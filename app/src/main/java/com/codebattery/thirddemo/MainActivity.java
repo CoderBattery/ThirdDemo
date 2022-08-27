8ce301c6edd8662e50c1b2bf7fcb5fa66fc59bed
wangchun <chunwang@gxatek.com>
2022-08-27 11:41:36 +0800

    first commit

---
+++ app/src/main/java/com/codebattery/thirddemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.codebattery.thirddemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
